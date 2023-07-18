80498501cbd8bbf422954e0700540dd21d8505c7
DanielTreep <danieltreep@live.nl>
2023-07-18 12:40:58 +0200

    updated vite config to go to localhost

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import dns from 'dns'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +16,7 @@ export default defineConfig({
     }
   },
   server: {
+    host: 'localhost',
     port: 8080
   }
 })
